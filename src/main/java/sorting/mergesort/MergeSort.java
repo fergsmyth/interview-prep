3f34f118a26b35cfc6bccb0236a96068be3029aa
Ferg Smyth <fergsmyth@gmail.com>
2019-10-09 15:37:04 +0100

    Completed solution for merge sort

--- src/main/java/sorting/mergesort/MergeSort.java
+++ src/main/java/sorting/mergesort/MergeSort.java
@@ -3,7 +3,7 @@ package sorting.mergesort;
 public class MergeSort {
 
     public static void main(String args[]) {
-        int[] arrayToSort = {12, 14, 16, 1, 3, 4, 17, 20};
+        int[] arrayToSort = {12, 14, 16, 1, 1, 3, 14, 20, 1, 3, 4, 17, 20};
         new MergeSort().mergeSort(arrayToSort, 0, arrayToSort.length - 1);
         for (int value : arrayToSort){
             System.out.print(value + " ");
@@ -20,31 +20,21 @@ public class MergeSort {
     }
 
     private void merge(int[] arrayToSort, int leftIndex, int midIndex, int rightIndex) {
-        System.out.println(String.format("Left %d, mid %d, right %d", leftIndex, midIndex, rightIndex));
         int leftLength = midIndex - leftIndex + 1;
-        System.out.println("Left length " + leftLength);
         int rightLength = rightIndex - midIndex;
-        System.out.println("Right length " + rightLength);
-        // rightLength : 8 - 4 = 4
 
-        // 0 - 3
         int[] leftArray = new int[leftLength];
-        // 4 - 7
         int[] rightArray = new int[rightLength];
 
-        // 0 -> 3
-        for(int i = 0; i < leftLength; i++) {
+        for(int i = 0; i < leftLength; i++)
             leftArray[i] = arrayToSort[leftIndex + i];
-        }
-        System.out.println("");
-        // 4 --> 7
-        for (int i = 0; i < rightLength; i++) {
-            rightArray[i] = arrayToSort[midIndex + i];
-        }
+
+        for (int i = 0; i < rightLength; i++)
+            rightArray[i] = arrayToSort[midIndex + i + 1];
 
         int leftInsertIndex = 0;
         int rightInsertIndex = 0;
-        for(int i = leftIndex; i < rightIndex; i++){
+        for(int i = leftIndex; i < rightIndex + 1; i++){
             if(leftInsertIndex < leftLength && rightInsertIndex < rightLength) {
                 if (leftArray[leftInsertIndex] < rightArray[rightInsertIndex]) {
                     arrayToSort[i] = leftArray[leftInsertIndex];
