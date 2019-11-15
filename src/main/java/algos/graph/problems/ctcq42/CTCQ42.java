4882905fc975794cbf50ee6c4adf13050c567f4c
Ferg Smyth <fergsmyth@gmail.com>
2019-11-15 14:55:50 +0000

    Small prefix change

--- src/main/java/algos/graph/problems/ctcq42/CTCQ42.java
+++ src/main/java/algos/graph/problems/ctcq42/CTCQ42.java
@@ -1,14 +1,11 @@
 package algos.graph.problems.ctcq42;
 
-import java.util.Arrays;
-
 public class CTCQ42 {
 
     public static void main(String[] args) {
         int[] valuesToInsert = {1,2,3,4,5,6,7,8,9,10};
         Tree mininalTree = Tree.createMininalTree(valuesToInsert);
         mininalTree.printTree();
-
     }
 
 }
